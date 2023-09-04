1e5d9ba0002bbcb5876ff0a50b6a1cffb7085017
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-09-04 14:58:06 +0200

    maj script

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const Person = function (firstName, birthYear, yearOld) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  let currentYear = new Date().getFullYear();
+  this.yearOld = currentYear - this.birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+const matilda = new Person('Matilda', 2017);
+// console.log(matilda);
+
+Person.prototype.calcAge = function(){
+    console.log(`another time, it's ${this.yearOld}` );
+}
+
+jonas.calcAge();
+matilda.calcAge();
