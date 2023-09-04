827b10f35ec875b889e32cd18aef633748c96554
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-09-04 15:48:06 +0200

    maj script

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
-'use strict';
+// 'use strict';
 
-const Person = function (firstName, birthYear, yearOld) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
-  let currentYear = new Date().getFullYear();
-  this.yearOld = currentYear - this.birthYear;
+// const Person = function (firstName, birthYear, yearOld) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+//   let currentYear = new Date().getFullYear();
+//   this.yearOld = currentYear - this.birthYear;
+// };
+
+// const jonas = new Person('Jonas', 1991);
+// const matilda = new Person('Matilda', 2017);
+// // console.log(matilda);
+
+// Person.prototype.calcAge = function(){
+//     console.log(`another time, it's ${this.yearOld}` );
+// }
+
+// jonas.calcAge();
+// matilda.calcAge();
+// Person.prototype.species = "homo sapiens";
+// console.log(jonas.species, matilda.species);
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const jonas = new Person('Jonas', 1991);
-const matilda = new Person('Matilda', 2017);
-// console.log(matilda);
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
 
-Person.prototype.calcAge = function(){
-    console.log(`another time, it's ${this.yearOld}` );
+Car.prototype.brake = function(){
+    this.speed -= 5;
+    console.log(this.speed);
 }
 
-jonas.calcAge();
-matilda.calcAge();
-Person.prototype.species = "homo sapiens";
-console.log(jonas.species, matilda.species);
+const theBmw = new Car("BMW", 120);
+theBmw.accelerate();
+theBmw.brake();
+theBmw.accelerate();
+theBmw.brake();
+theBmw.accelerate();
+theBmw.brake();
+theBmw.accelerate();
+theBmw.brake();
+
+const theMercedes = new Car("Metcedes", 95);
+theMercedes.accelerate();
+theMercedes.brake();
+theMercedes.accelerate();
+theMercedes.brake();
